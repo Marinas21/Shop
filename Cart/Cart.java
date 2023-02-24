2f793386a19768dd956f346d2ed2514fa1fdacc6
Marinas98 <andreimarinas238@yahoo.com>
2023-02-24 14:33:48 +0200

    Updates

---
+++ Cart/Cart.java
@@ -0,0 +1,43 @@
+package com.Andrei.ShopWebService.Cart;
+
+public class Cart {
+
+    private int id;
+    private int userId;
+
+    public Cart(int id, int userId) {
+        this.id = id;
+        this.userId = userId;
+    }
+
+    public Cart(int userId) {
+        this.userId = userId;
+    }
+
+    public Cart() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public String toString() {
+        return "Cart{" +
+                "id=" + id +
+                ", userId=" + userId +
+                '}';
+    }
+}
---
+++ Cart/CartData.java
@@ -0,0 +1,49 @@
+package com.Andrei.ShopWebService.Cart;
+
+import com.Andrei.ShopWebService.User.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+//This class represents all the communications with the database on a specific cart
+public class CartData {
+
+    //This methode send a request to create a new cart for a specific user
+    public static String createNewCart(JdbcTemplate jdbcTemplate, String userId){
+        jdbcTemplate.execute("insert into shop.cart values(null,"+userId+")");
+        return "Cart created";
+    }
+    //This methode send a request to delete a cart based on id
+    public static String deleteCart(JdbcTemplate jdbcTemplate, String id){
+        jdbcTemplate.execute("delete from shop.cart where id="+id+"");
+        return "Cart deleted";
+    }
+
+    //This methode send a request to get a cart based on id
+    public static Cart getCartById(JdbcTemplate jdbcTemplate, String id){
+        List<Cart> list=jdbcTemplate.query("select * from shop.cart where id="+id+"",(rs, rowNum) ->
+                new Cart(
+                        rs.getInt("id"),
+                        rs.getInt("userId")
+                )
+        );
+        if(list.size()==1){
+            return list.get(0);
+        }
+        return null;
+    }
+
+    //This methode send a request to get a cart based on user id
+    public static Cart getCartByUserId(JdbcTemplate jdbcTemplate, String userId){
+        List<Cart> list=jdbcTemplate.query("select * from shop.cart where userId="+userId+"",(rs, rowNum) ->
+                new Cart(
+                        rs.getInt("id"),
+                        rs.getInt("userId")
+                )
+        );
+        if(list.size()==1){
+            return list.get(0);
+        }
+        return null;
+    }
+}
